ff83b3d4b8e66cbf7148b4394b042fe258b8b125
Alzira25 <alzirabarretto@gmail.com>
2017-03-24 17:41:57 +0530

    Create func.php

---
+++ functions/func.php
@@ -0,0 +1,17 @@
+<?php
+
+include("includes/database_connect.php");
+	
+//getting the user IP address
+function getIp() {
+    $ip = $_SERVER['REMOTE_ADDR'];
+ 
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+ 
+    return $ip;
+}
+
